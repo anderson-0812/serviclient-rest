9c47e852e4817cde5352a93132b3ca33d3ab24cf
Anderson Jimenez <andersonmurdog@gmail.com>
2019-11-11 22:53:23 -0500

    Crud de itemRubric

--- routes/itemRubric.js
+++ routes/itemRubric.js
@@ -40,3 +40,69 @@ app.get('itemRubric/:id',(req,res)=>{
         })
     });
 });
+
+// Put edit by id
+app.put('/itemRubric/:id',(req,res)=>{
+    let id = req.params.id;
+    let body= req.body;
+
+    let itemRubricParaEditar = new itemRubric({
+        evaluationRubric: body.evaluationRubric,
+        evaluationItem: body.evaluationItem
+    });
+
+    itemRubric.findByIdAndUpdate(id,itemRubricParaEditar,{
+        new: true,
+        runValidators:true
+    },(err, itemRubricDB)=>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if(!itemRubricDB){
+            return res.status(400).json({
+                ok: false,
+                itemRubricDB
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            itemRubricDB
+        });
+    });
+});
+
+// delete 
+app.delete('/itemRubric/:id',(req,res)=>{
+    let id = req.params.id;
+    let itemRubricDelete = {
+        state: false
+    }
+
+    itemRubric.findByIdAndUpdate(id,itemRubricDelete,{
+        new:true,
+        runValidators:true
+    },(err, itemRubricDB)=>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if(!itemRubricDB){
+            return res.status(400).json({
+                ok: false,
+                itemRubricDB
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            itemRubricDB
+        });
+    });
+});
+
+
+module.exports = app;
